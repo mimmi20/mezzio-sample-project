ef2f7bd69bba5b98cec4fefe5b49843b5d462fd8
Thomas Müller <mimmi20@live.de>
2024-09-07 10:03:59 +0200

    update eslint config

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -10,60 +10,71 @@ module.exports = function (ctx) {
         style: 'expanded',
         alertAscii: true,
         alertColor: true,
-        verbose: ctx.env !== 'production'
+        verbose: ctx.env !== 'production',
       }),
-      require('postcss-import')({root: root}),
+      require('postcss-import')({ root: root }),
       require('postcss-strip-inline-comments'),
       require('postcss-will-change'),
-      require('postcss-color-rebeccapurple')({preserve: true}),
-      require('css-declaration-sorter')({order: 'smacss', keepOverrides: true}),
+      require('postcss-color-rebeccapurple')({ preserve: true }),
+      require('css-declaration-sorter')({ order: 'smacss', keepOverrides: true }),
       require('postcss-color-rgba-fallback'),
       //require('postcss-prefixwrap')('.base-container'),// no prefix here, will be added in the next step
       //require('postcss-prefix-selector')(),
       //require('postcss-modules'),
+      require('postcss-rtl-logical-properties'),
       require('postcss-preset-env')({
-        'stage': 2,
-        'minimumVendorImplementations': 0,
-        'features': {
-          'any-link-pseudo-class': {'preserve': true}, // allows using ":any-link" pseudo class
+        stage: 2,
+        minimumVendorImplementations: 0,
+        features: {
+          'any-link-pseudo-class': { preserve: true }, // allows using ":any-link" pseudo class
           'cascade-layers': true,
-          'clamp': {'precalculate': true}, // allows using "clamp()" function
-          'color-function': {'preserve': true}, // allows using "color()" function
-          'custom-media-queries': {'preserve': true},
-          'custom-properties': {'preserve': true},
-          'custom-selectors': {'preserve': true},
-          'dir-pseudo-class': {'preserve': true}, // allows using ":dir" pseudo class
-          'display-two-values': {'preserve': true},
-          'focus-visible-pseudo-class': {'preserve': true}, // allows using ":focus-visible" pseudo class
-          'focus-within-pseudo-class': {'preserve': true}, // allows using ":focus-within" pseudo class
-          'font-format-keywords': {'preserve': true},
-          'gap-properties': {'preserve': true},
-          'hexadecimal-alpha-notation': {'preserve': true},
-          'image-set-function': {'preserve': true, 'onInvalid': 'warning'}, // allows using "image-set()" function
+          clamp: { precalculate: true }, // allows using "clamp()" function
+          'color-function': { preserve: true }, // allows using "color()" function
+          'custom-media-queries': { preserve: true },
+          'custom-properties': { preserve: true },
+          'custom-selectors': { preserve: true },
+          'dir-pseudo-class': { preserve: true }, // allows using ":dir" pseudo class
+          'display-two-values': { preserve: true },
+          'float-clear-logical-values': true,
+          'focus-visible-pseudo-class': { preserve: true }, // allows using ":focus-visible" pseudo class
+          'focus-within-pseudo-class': { preserve: true }, // allows using ":focus-within" pseudo class
+          'font-format-keywords': { preserve: true },
+          'gap-properties': { preserve: true },
+          'hexadecimal-alpha-notation': { preserve: true },
+          'image-set-function': { preserve: true, onInvalid: 'warning' }, // allows using "image-set()" function
           'is-pseudo-class': false, // allows using ":is" pseudo class
+          'logical-overflow': { preserve: true },
+          'logical-overscroll-behavior': { preserve: true },
+          'logical-properties-and-values': { preserve: true },
+          'logical-resize': { preserve: true },
+          'logical-viewport-units': { preserve: true },
           'media-query-ranges': true,
-          'nested-calc': {'preserve': true},
-          'nesting-rules': {'noIsPseudoSelector': false},
-          'not-pseudo-class': true,// allows using ":not" pseudo class
-          'opacity-percentage': {'preserve': true},
-          'overflow-property': {'preserve': true},
-          'overflow-wrap-property': {'method': 'copy'},
-          'rebeccapurple-color': {'preserve': false},
-          'system-ui-font-family': {'preserve': true},
-          'text-decoration-shorthand': {'preserve': true},
-          'trigonometric-functions': {'preserve': true},
-          'unset-value': {'preserve': true}
+          'nested-calc': { preserve: true },
+          'nesting-rules': { noIsPseudoSelector: false },
+          'not-pseudo-class': true, // allows using ":not" pseudo class
+          'opacity-percentage': { preserve: true },
+          'overflow-property': { preserve: true },
+          'overflow-wrap-property': { method: 'copy' },
+          'rebeccapurple-color': { preserve: false },
+          'system-ui-font-family': { preserve: true },
+          'text-decoration-shorthand': { preserve: true },
+          'trigonometric-functions': { preserve: true },
+          'unset-value': { preserve: true },
         },
-        'autoprefixer': {'grid': true},
-        'preserve': true
+        autoprefixer: { grid: true },
+        preserve: true,
       }),
-      ctx.env === 'production' ? require('cssnano')({
-        preset: 'default',
-        safe: true,
-        calc: false,
-        minifyFontWeight: false,
-        precision: 2
-      }) : false,
+      require('postcss-pxtorem')({ mediaQuery: true, propList: ['*'] }),
+      require('css-declaration-sorter')({ keepOverrides: true, order: 'smacss' }),
+      ctx.env === 'production'
+        ? require('cssnano')({
+          preset: 'default',
+          safe: true,
+          calc: false,
+          minifyFontWeight: false,
+          precision: 2,
+        })
+        : false,
     ],
   };
 };
