fe8a0f91346553e52f2d689a5d4a712ac003fd22
Thomas Müller <mimmi20@live.de>
2025-01-03 13:50:02 +0100

    update dependencies

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -16,6 +16,8 @@ import postcssPseudoColons from 'postcss-pseudo-element-colons';
 import autoprefixer from 'autoprefixer';
 import postcssDiscardComments from 'postcss-discard-comments';
 import postcssImport from 'postcss-import';
+import postColorConverter from 'postcss-color-converter';
+import postcssLightningcss from 'postcss-lightningcss';
 
 export default function (ctx) {
   const root = process.cwd();
@@ -29,6 +31,7 @@ export default function (ctx) {
         selectors: ['before', 'after', 'first-letter', 'first-line'],
         'colon-notation': 'double',
       }),
+      postColorConverter({ outputColorFormat: 'rgb', ignore: ['rgb', 'hsl'], alwaysAlpha: true }),
       postcssPxtorem({
         propList: ['*'],
         replace: true,
@@ -101,6 +104,19 @@ export default function (ctx) {
         enableClientSidePolyfills: false,
         debug: ctx.env !== 'production',
         logical: false,
+      }),
+      postcssLightningcss({
+        lightningcssOptions: {
+          minify: false,
+          sourceMap: ctx.env !== 'production',
+          cssModules: false,
+          // Individually enable various drafts
+          drafts: {
+            // Enable css nesting (default: undefined)
+            nesting: true,
+            customMedia: true,
+          },
+        }
       }),
       postcssColorRgbaFallback,
       autoprefixer({
